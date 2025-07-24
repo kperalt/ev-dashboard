b3c78b0a9e2b63088a6503ada50ff84e7cd656ee
kperalt1 <kperalt1@swarthmore.edu>
2025-07-24 12:35:09 -0400

    added date filter and update EV sales chart

--- app.py
+++ app.py
@@ -21,7 +21,24 @@ df_long['Company'] = df_long['Company'].str.replace('_sales', '').str.capitalize
 
 # Ready Streamlit dashboard
 st.title("EV Sales Dashboard")
-st.dataframe(df_long)
 
+# Date filters in sidebar
+min_date = df_long['Date'].min()
+max_date = df_long['Date'].max()
+
+start_date = st.sidebar.date_input('Start date', min_date)
+end_date = st.sidebar.date_input('End date', max_date)
+
+if start_date > end_date:
+    st.error("Error: End date must be after start date.")
+else:
+    # Filter data by date range
+    filtered_data = df_long[(df_long['Date'] >= pd.to_datetime(start_date)) &
+                            (df_long['Date'] <= pd.to_datetime(end_date))]
+
+st.write(f"Data from {start_date} to {end_date}")
+st.dataframe(filtered_data)
+
+# Line chart
 chart_data = df_long.pivot(index='Date', columns='Company', values='Sales')
 st.line_chart(chart_data)
